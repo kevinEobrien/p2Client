7a385d236f260788e084a9a867aa4c5c76824097
KEVIN OBRIEN <kevinobrien@KEVINs-MacBook-Air.local>
2018-01-24 16:22:17 -0700

    changed path to js

---
+++ README.md
@@ -0,0 +1,4 @@
+#InpireArtHub
+
+##Project Purpose:
+This project creates a place to hunt down inspriring videos and other resources for beginners.
--- js/home.js
+++ js/home.js
@@ -7,22 +7,23 @@ random.addEventListener("click", navigateRandom);
 var editor = document.querySelector(".editor");
 editor.addEventListener("click", navigateEditor);
 
-function navigateList (event){
+function navigateList(event) {
+  console.log("it works");
   event.preventDefault();
   localStorage.setItem("searchterm", event.target[0].value);
   localStorage.setItem("resource", event.target[1].value);
   location.href = "list.html";
 }
 
-function navigateRandom (event){
+function navigateRandom(event) {
   event.preventDefault();
   location.href = "random.html";
 }
-function navigateEditor(event){
+function navigateEditor(event) {
   event.preventDefault();
   location.href = "editor.html";
 }
-function navigateSuggest (event){
+function navigateSuggest(event) {
   event.preventDefault();
   location.href = "suggest.html";
 }
